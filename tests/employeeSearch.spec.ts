3bfbc741333a31848a432723bee2b4d6b297b3ff
“ajay500ranajava” <ajay44314@gmail.com>
2025-08-21 18:13:58 +0530

    latest commit with passed comment

--- tests/employeeSearch.spec.ts
+++ tests/employeeSearch.spec.ts
@@ -1,3 +1,4 @@
+import testData from "../test-data/testData.json";
 import { test, expect } from '@playwright/test';
 
 test('Search employee and validate details in OrangeHRM', async ({ page }) => {
@@ -5,15 +6,15 @@ test('Search employee and validate details in OrangeHRM', async ({ page }) => {
   //await page.goto('https://www.google.com', { waitUntil: 'domcontentloaded' });
 //await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 test.setTimeout(120000); // set higher test timeout
-await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', {
+await page.goto(testData.url.orangeHrmUrl, {
   waitUntil: 'networkidle',
   timeout: 90000
 });
 ;
 
 //  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.fill('input[name="username"]', 'Admin');
-  await page.fill('input[name="password"]', 'admin123');
+  await page.fill('input[name="username"]', testData.validUser.username);
+  await page.fill('input[name="password"]', testData.validUser.password);
   await page.click('button[type="submit"]');
 
   // 2. Navigate to PIM -> Employee List tab.
@@ -21,8 +22,12 @@ await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/lo
   await page.getByRole('link', { name: 'Employee List' }).click();
 
   // 3. Enter a valid employee name in "Employee Name" field
-  const searchEmployee = "Linda Anderson"; // employee to search
-  await page.fill('input[placeholder="Type for hints..."]', searchEmployee);
+  const searchEmployee = "Ajay Rana"; // employee to search Linda Anderson
+  await page.fill('input[placeholder="Type for hints..."]', testData.employee.searchName);
+  
+
+  /*const empId = "450"; // 
+  await page.fill('input.oxd-input oxd-input--active', empId);*/
 
   // 4. Click on Search button
   await page.click('button:has-text("Search")');
@@ -45,6 +50,7 @@ await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/lo
   }
 
   // 7. Click on the first returned result
+  //await page.locator(`//div[text()='${empId}']`).click();
   const firstRow = recordFound.first();
   //first comment added -2
   await firstRow.click();
@@ -52,10 +58,18 @@ await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/lo
   // 8. Verify Personal Details screen with Employee Name displayed
   await expect(page.locator('h6:has-text("Personal Details")')).toBeVisible();
 
-  // Just validate that First Name input is not empty (instead of hardcoding wrong value)
+  // Just validate that First Name input is not empty 
   const firstNameField = page.locator('input[name="firstName"]');
   await expect(firstNameField).not.toHaveValue('');
 
+ 
+  // 9. Validate Employee ID field is not empty
+  /*const empIdField = page.locator('input[name="employeeId"]');
+  await expect(empIdField).toHaveValue(empId);*/
+
+  //console.log("✅ Employee ID matched:", await empIdField.inputValue());
+
   // Optionally log the actual value
   console.log("Employee first name is:", await firstNameField.inputValue());
+  //tc passed
 });
